a98036c5fe608274daf542a273f2d255a830c3a7
RomanKrass <krasr1@bfh.ch>
2024-07-07 16:07:05 +0200

    added exchange rate

--- Project.R
+++ Project.R
@@ -1,3 +1,4 @@
+# Load required libraries
 library(shiny)
 library(shinydashboard)
 library(dplyr)
@@ -6,9 +7,15 @@ library(leaflet)
 library(tidygeocoder)
 library(tidyverse)
 library(viridis)
+library(shinycssloaders)
+library(rvest)
 
 # Define the URL for the CSV file
 filepath <- "https://raw.githubusercontent.com/sushantag9/Supermarket-Sales-Data-Analysis/master/supermarket_sales%20-%20Sheet1.csv"
+
+# URL for the exchange rate
+UrlER_USD_MMK <- "https://www.xe.com/de/currencyconverter/convert/?Amount=1&From=USD&To=MMK"
+UrlER_USD_CHF <- "https://www.xe.com/de/currencyconverter/convert/?Amount=1&From=USD&To=CHF"
 
 # Define the header
 header <- dashboardHeader(
@@ -21,7 +28,8 @@ sidebar <- dashboardSidebar(
     menuItem("Dashboard", tabName = "dashboard"),
     menuItem("Map", tabName = "map"),
     menuItem("Inputs", tabName = "inputs"),
-    menuItem("Charts", tabName = "charts")
+    menuItem("Charts", tabName = "charts"),
+    menuItem("Exchange Rate", tabName = "exchange")
   )
 )
 
@@ -50,7 +58,11 @@ body <- dashboardBody(
       tabBox(
         title = "Map",
         width = 12,
-        tabPanel("Map", leafletOutput("sales_map", height = 800))
+        tabPanel(
+          "Map",
+          "Store locations on a map",
+          leafletOutput("sales_map", height = 800) %>% withSpinner()
+        )
       )
     ),
     tabItem(
@@ -78,7 +90,8 @@ body <- dashboardBody(
               multiple = TRUE
             )
           ),
-          plotOutput("vis_city")
+          plotOutput("vis_city"),
+          plotOutput("vis_city_date")
         ),
         tabPanel(
           "Custom",
@@ -110,6 +123,29 @@ body <- dashboardBody(
             )
           ),
           plotOutput("custom")
+        ),
+        tabPanel(
+          "Reviews",
+          inputPanel(
+            selectInput("member_type2", "Select a member type",
+              choices = "",
+              selected = "Normal",
+              multiple = TRUE
+            )
+          ),
+          plotOutput("vis_ratings")
+        ),
+      )
+    ),
+    tabItem(
+      tabName = "exchange",
+      tabBox(
+        title = "Exchange Rates",
+        width = 12,
+        tabPanel(
+          "USD to MMK",
+          selectInput("exchange", "Select an exchange rate", choices = c("USD to MMK", "USD to CHF"), selected = "USD to MMK"),
+          textOutput("exchangeRate_Text")
         )
       )
     )
@@ -174,7 +210,7 @@ server <- function(input, output, session) {
         y = "Total Sales"
       ) +
       scale_y_continuous() +
-      theme_minimal() +
+      theme_minimal(base_size = 14) +
       scale_color_viridis(discrete = TRUE)
   })
 
@@ -191,7 +227,26 @@ server <- function(input, output, session) {
         y = "Total Sales"
       ) +
       scale_y_continuous() +
-      theme_minimal() +
+      theme_minimal(base_size = 14) +
+      scale_color_viridis(discrete = TRUE, option = "C")
+  })
+
+  output$vis_city_date <- renderPlot({
+    sales_data() %>%
+      filter(City %in% input$city) %>%
+      mutate(Date = as.Date(Date, format = "%m/%d/%Y")) %>%
+      arrange(Date) %>%
+      group_by(City, Date) %>%
+      summarise(Total = sum(Total), .groups = "drop") %>%
+      ggplot(aes(x = Date, y = Total, color = City)) +
+      geom_line() +
+      labs(
+        title = "Amount of sales per city",
+        x = "City",
+        y = "Total Sales"
+      ) +
+      scale_y_continuous() +
+      theme_minimal(base_size = 14) +
       scale_color_viridis(discrete = TRUE)
   })
 
@@ -209,14 +264,57 @@ server <- function(input, output, session) {
       ggplot(aes(x = City, y = Total, fill = City)) +
       geom_col() +
       labs(
-        title = "Amount of sales per city",
+        title = "Amount of sales per city with filters",
         x = "City",
         y = "Total Sales"
       ) +
       scale_y_continuous() +
-      theme_minimal()
+      theme_minimal(base_size = 14)
   })
 
+  output$vis_ratings <- renderPlot({
+    sales_data() %>%
+      filter(`Customer.type` %in% input$member_type2) %>%
+      group_by(City) %>%
+      summarise(Average_Rating = mean(Rating), .groups = "drop") %>%
+      ggplot(aes(x = City, y = Average_Rating, fill = City)) +
+      geom_col() +
+      labs(
+        title = "Average rating per city",
+        x = "City",
+        y = "Rating"
+      ) +
+      scale_y_continuous() +
+      theme_minimal(base_size = 14) +
+      scale_color_viridis(discrete = TRUE)
+  })
+
+  # Reactive expression to fetch exchange rate
+  exchangeRate <- reactive({
+    # Invalidate this reactive expression every 60 seconds to update the rate
+    invalidateLater(60000, session)
+    # Determine the URL based on the selected currency pair
+    URLExchangeRate <- switch(input$exchange,
+      "USD to MMK" = UrlER_USD_MMK,
+      "USD to CHF" = UrlER_USD_CHF,
+      NULL
+    )
+    req(URLExchangeRate) # Ensure a URL is selected
+
+    page <- rvest::read_html(URLExchangeRate)
+    rate <- page %>%
+      rvest::html_nodes(".sc-295edd9f-1") %>%
+      rvest::html_text()
+
+    # Return the fetched rate
+    rate
+  })
+
+  # Output the exchange rate
+  output$exchangeRate_Text <- renderText({
+    rate <- exchangeRate()
+    paste("1 USD dollar is currently: ", rate)
+  })
   # Observe changes in sales_data and update selectInput accordingly
   observe({
     # Ensure sales_data is available before trying to use it
@@ -247,7 +345,6 @@ server <- function(input, output, session) {
       selected = "Yangon"
     )
 
-
     updateSelectInput(session, "gender",
       choices = unique(sales_data()$Gender),
       selected = "Male"
@@ -259,6 +356,11 @@ server <- function(input, output, session) {
     )
 
     updateSelectInput(session, "member_type",
+      choices = unique(sales_data()$Customer.type),
+      selected = "Normal"
+    )
+
+    updateSelectInput(session, "member_type2",
       choices = unique(sales_data()$Customer.type),
       selected = "Normal"
     )

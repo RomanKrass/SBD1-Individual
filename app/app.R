8f4e1c2cddeaea34a8b24c44dd47ea2081e9f9af
RomanKrass <krasr1@bfh.ch>
2024-07-13 11:33:55 +0200

    update

--- app/app.R
+++ app/app.R
@@ -4,6 +4,8 @@ library(shinydashboard)
 library(dplyr)
 library(DT)
 library(leaflet)
+library(leaflet)
+library(leaflet.extras)
 library(tidygeocoder)
 library(tidyverse)
 library(viridis)
@@ -16,6 +18,7 @@ filepath <- "https://raw.githubusercontent.com/sushantag9/Supermarket-Sales-Data
 # URL for the exchange rate
 UrlER_USD_MMK <- "https://www.xe.com/de/currencyconverter/convert/?Amount=1&From=USD&To=MMK"
 UrlER_USD_CHF <- "https://www.xe.com/de/currencyconverter/convert/?Amount=1&From=USD&To=CHF"
+UrlER_USD_EUR <- "https://www.xe.com/de/currencyconverter/convert/?Amount=1&From=USD&To=EUR"
 
 # Define the header
 header <- dashboardHeader(
@@ -25,11 +28,10 @@ header <- dashboardHeader(
 # Define the sidebar
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard"),
-    menuItem("Map", tabName = "map"),
-    menuItem("Inputs", tabName = "inputs"),
     menuItem("Charts", tabName = "charts"),
-    menuItem("Exchange Rate", tabName = "exchange")
+    menuItem("Map", tabName = "map"),
+    menuItem("Exchange Rate", tabName = "exchange"),
+    menuItem("Inputs", tabName = "inputs")
   )
 )
 
@@ -45,22 +47,13 @@ body <- dashboardBody(
       )
     ),
     tabItem(
-      tabName = "dashboard",
-      tabBox(
-        title = "Dashboard",
-        width = 12,
-        tabPanel("Fun Facts 1", "Anything you want"),
-        tabPanel("Fun Fact 2", "blabla")
-      )
-    ),
-    tabItem(
       tabName = "map",
       tabBox(
         title = "Map",
         width = 12,
         tabPanel(
           "Map",
-          "Store locations on a map",
+          "All store locations",
           leafletOutput("sales_map", height = 800) %>% withSpinner()
         )
       )
@@ -143,9 +136,9 @@ body <- dashboardBody(
         title = "Exchange Rates",
         width = 12,
         tabPanel(
-          "USD to MMK",
-          selectInput("exchange", "Select an exchange rate", choices = c("USD to MMK", "USD to CHF"), selected = "USD to MMK"),
-          textOutput("exchangeRate_Text")
+          "Current Exchange Rates",
+          selectInput("exchange", "Select an exchange rate", choices = c("USD to MMK", "USD to CHF", "USD to EUR"), selected = "USD to MMK"),
+          textOutput("exchangeRate_Text") %>% withSpinner()
         )
       )
     )
@@ -167,7 +160,7 @@ server <- function(input, output, session) {
     read.csv(url(filepath))
   })
 
-  # Geocode cities
+  # Get geocode of the cities
   geocoded_data <- reactive({
     data <- sales_data() %>%
       distinct(City)
@@ -188,6 +181,7 @@ server <- function(input, output, session) {
     leaflet(geo_data) %>%
       addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
       addTiles() %>%
+      addResetMapButton() %>%
       addCircleMarkers(
         ~long, ~lat,
         label = ~City,
@@ -197,6 +191,7 @@ server <- function(input, output, session) {
       )
   })
 
+  # Render income plot
   output$vis_income <- renderPlot({
     sales_data() %>%
       filter(Payment %in% input$payment_methode) %>%
@@ -214,6 +209,7 @@ server <- function(input, output, session) {
       scale_color_viridis(discrete = TRUE)
   })
 
+  # Render plots by city
   output$vis_city <- renderPlot({
     sales_data() %>%
       filter(City %in% input$city) %>%
@@ -231,6 +227,7 @@ server <- function(input, output, session) {
       scale_color_viridis(discrete = TRUE, option = "C")
   })
 
+  # Render plots by city and date
   output$vis_city_date <- renderPlot({
     sales_data() %>%
       filter(City %in% input$city) %>%
@@ -250,6 +247,7 @@ server <- function(input, output, session) {
       scale_color_viridis(discrete = TRUE)
   })
 
+  # Render customised plot
   output$custom <- renderPlot({
     sales_data() %>%
       filter(
@@ -272,6 +270,7 @@ server <- function(input, output, session) {
       theme_minimal(base_size = 14)
   })
 
+  # Render ratings plot
   output$vis_ratings <- renderPlot({
     sales_data() %>%
       filter(`Customer.type` %in% input$member_type2) %>%
@@ -293,12 +292,15 @@ server <- function(input, output, session) {
   exchangeRate <- reactive({
     # Invalidate this reactive expression every 60 seconds to update the rate
     invalidateLater(60000, session)
+
     # Determine the URL based on the selected currency pair
     URLExchangeRate <- switch(input$exchange,
       "USD to MMK" = UrlER_USD_MMK,
       "USD to CHF" = UrlER_USD_CHF,
+      "USD to EUR" = UrlER_USD_EUR,
       NULL
     )
+
     req(URLExchangeRate) # Ensure a URL is selected
 
     page <- rvest::read_html(URLExchangeRate)
@@ -315,6 +317,7 @@ server <- function(input, output, session) {
     rate <- exchangeRate()
     paste("1 USD dollar is currently: ", rate)
   })
+
   # Observe changes in sales_data and update selectInput accordingly
   observe({
     # Ensure sales_data is available before trying to use it
@@ -331,35 +334,43 @@ server <- function(input, output, session) {
       selected = "Ewallet"
     )
 
+    # Update the selectInput with the unique payment methods
     updateSelectInput(session, "payment_methode2",
       choices = unique_payments,
       selected = "Ewallet"
     )
 
+    # Update the selectInput with the unique cities
     updateSelectInput(session, "city",
       choices = unique(sales_data()$City),
       selected = "Yangon"
     )
+
+    # Update the selectInput with the unique cities
     updateSelectInput(session, "city2",
       choices = unique(sales_data()$City),
       selected = "Yangon"
     )
 
+    # Update the selectInput with the gender values
     updateSelectInput(session, "gender",
       choices = unique(sales_data()$Gender),
       selected = "Male"
     )
 
+    # Update the selectInput with the product line values
     updateSelectInput(session, "product_line",
       choices = unique(sales_data()$Product.line),
       selected = "Health and beauty"
     )
 
+    # Update the selectInput with the member type values
     updateSelectInput(session, "member_type",
       choices = unique(sales_data()$Customer.type),
       selected = "Normal"
     )
 
+    # Update the selectInput with the member type values
     updateSelectInput(session, "member_type2",
       choices = unique(sales_data()$Customer.type),
       selected = "Normal"
